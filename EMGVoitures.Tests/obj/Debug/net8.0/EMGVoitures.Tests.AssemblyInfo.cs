2db3d6b6066eb3207a335ab5f808db9d385e2136
AnewarFlutter <mohamedattandadamala@esp.sn>
2025-01-31 22:27:25 +0000

    Ajout de l'authentification Azure dans le workflow de déploiement et mise à jour des informations d'assemblage pour les projets.

--- EMGVoitures.Tests/obj/Debug/net8.0/EMGVoitures.Tests.AssemblyInfo.cs
+++ EMGVoitures.Tests/obj/Debug/net8.0/EMGVoitures.Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EMGVoitures.Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6b4f2de94d96b61ce166e51d8bc1f235d2b879e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0a9bc602f558f849eec325895fdad6163b3b0f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EMGVoitures.Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EMGVoitures.Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
